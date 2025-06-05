2f2862057feafee90ad81a883139dafd87f16385
beliy016 <beliy1@ro.ru>
2025-06-05 11:41:21 +0300

    Solution-4

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServImpl;
+import web.service.CarServ;
 
 @Controller
 public class CarsController {
 
     @Autowired
-    private CarServImpl service;
+    private CarServ service;
 
     @GetMapping(value = "/cars")
     public String showCars(@RequestParam(value="count", required = false, defaultValue = "5") Integer count, Model model) {
